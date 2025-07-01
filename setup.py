95290043f5ea6e746bd9209b6d925fa72f92d102
Shrey Goel <shrey.first@gmail.com>
2025-07-01 20:27:07 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FLaT',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Shrey Goel',
+    author_email='shrey.goel@duke.edu'
+)
